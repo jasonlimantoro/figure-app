b6c7c34eb7a29141842d1d749b3a93fa53ae895f
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-10-25 02:51:01 +0800

    add slant properties for Cone

--- src/figure/Cone.java
+++ src/figure/Cone.java
@@ -3,6 +3,7 @@ package figure;
 public class Cone extends Shape
 {
     private Circle base;
+    private double slant;
     private double height;
     private double radius;
 
@@ -10,12 +11,13 @@ public class Cone extends Shape
     {
         super(dimensions);
         this.base = new Circle(radius, dimensions);
+        this.slant = Math.sqrt(Math.pow(this.height, 2) + Math.pow(this.radius, 2));
         this.radius = radius;
         this.height = height;
     }
 
     public double calculateArea()
     {
-        return base.calculateArea() + Math.PI * this.radius * Math.sqrt(Math.pow(this.height, 2) + Math.pow(this.radius, 2));
+        return base.calculateArea() + Math.PI * this.radius * this.slant;
     }
 }
