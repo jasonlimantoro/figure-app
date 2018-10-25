fbfa857120ec0fd2740d3876612c02c6846ee56a
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-10-25 10:06:57 +0800

    remove slant property

--- src/figure/Cone.java
+++ src/figure/Cone.java
@@ -3,7 +3,6 @@ package figure;
 public class Cone extends Shape
 {
     private Circle base;
-    private double slant;
     private double height;
     private double radius;
 
@@ -13,11 +12,11 @@ public class Cone extends Shape
         this.base = new Circle(radius, dimensions);
         this.radius = radius;
         this.height = height;
-        this.slant = Math.sqrt(Math.pow(this.height, 2) + Math.pow(this.radius, 2));
     }
 
     public double calculateArea()
     {
-        return base.calculateArea() + Math.PI * this.radius * this.slant;
+        double slant = Math.sqrt(this.height * this.height + this.radius * this.radius);
+        return base.calculateArea() + Math.PI * this.radius * slant;
     }
 }
